8997312a9d6b4cdcd1df40934fa4cfd201cfda4b
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-07-28 07:54:04 +0000

    Update to title

--- src/components/layout/title.tsx
+++ src/components/layout/title.tsx
@@ -9,7 +9,7 @@ import { Logo } from "./logo";
 
 const { useToken } = theme;
 
-const name = "s";
+const name = "CrecheSpots";
 
 export const Title: React.FC<RefineLayoutThemedTitleProps> = ({
   collapsed,
