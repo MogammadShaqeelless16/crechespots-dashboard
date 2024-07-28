13ab321ea96de59f6d51c4459dd8cc77aa2f1faf
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-07-28 00:05:51 +0000

    Update

--- src/routes/chat/wrapper.tsx
+++ src/routes/chat/wrapper.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-
 import { CreateButton } from "@refinedev/antd";
 import { useNavigation } from "@refinedev/core";
-
-
 
 export const ChatPageWrapper: React.FC<React.PropsWithChildren> = ({
   children,
@@ -14,8 +11,12 @@ export const ChatPageWrapper: React.FC<React.PropsWithChildren> = ({
   );
 
   return (
-    <div className="page-container">
-          <iframe src="https://ayoba-hackathon-socket.onrender.com/"></iframe>
+    <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+      <iframe 
+        src="https://ayoba-hackathon-socket.onrender.com/" 
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Ayoba Hackathon Socket"
+      ></iframe>
     </div>
   );
 };
